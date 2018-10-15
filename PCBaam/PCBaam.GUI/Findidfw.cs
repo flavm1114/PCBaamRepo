4839cd93d8dccb1a6b5a3af3ac6f508508453e18
ansungtae <41276504+ansungtae@users.noreply.github.com>
2018-10-15 15:25:48 +0900

    mainform 변경

--- PCBaam/PCBaam.GUI/Findidfw.cs
+++ PCBaam/PCBaam.GUI/Findidfw.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace PCBaam.GUI
 {
-    public partial class Form1 : Form
+    public partial class Findidfw : Form
     {
-        public Form1()
+        public Findidfw()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ExitButton_Click(object sender, EventArgs e)
-        {
-            Close();
         }
     }
 }
--- PCBaam/PCBaam.GUI/Form1.Designer.cs
+++ PCBaam/PCBaam.GUI/Form1.Designer.cs
@@ -28,13 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.IDLabel = new System.Windows.Forms.Label();
+            this.FindIdPw = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::PCBaam.GUI.Properties.Resources.mainformscreen_jpg1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.ExitButton);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.IDLabel);
+            this.panel1.Controls.Add(this.FindIdPw);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SignUpButton);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.BackColor = System.Drawing.Color.Black;
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ExitButton.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.ExitButton.Location = new System.Drawing.Point(631, 381);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(140, 39);
+            this.ExitButton.TabIndex = 16;
+            this.ExitButton.Text = "종료";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button1.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button1.Location = new System.Drawing.Point(653, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 96);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.BackColor = System.Drawing.Color.Transparent;
+            this.IDLabel.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.IDLabel.Font = new System.Drawing.Font("함초롬바탕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.IDLabel.ForeColor = System.Drawing.Color.Black;
+            this.IDLabel.Location = new System.Drawing.Point(350, 281);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(138, 35);
+            this.IDLabel.TabIndex = 9;
+            this.IDLabel.Text = "아이디(ID)";
+            // 
+            // FindIdPw
+            // 
+            this.FindIdPw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FindIdPw.BackColor = System.Drawing.Color.Black;
+            this.FindIdPw.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FindIdPw.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.FindIdPw.Location = new System.Drawing.Point(485, 381);
+            this.FindIdPw.Name = "FindIdPw";
+            this.FindIdPw.Size = new System.Drawing.Size(140, 39);
+            this.FindIdPw.TabIndex = 15;
+            this.FindIdPw.Text = "ID/PW 찾기";
+            this.FindIdPw.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.label1.Font = new System.Drawing.Font("함초롬바탕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(311, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 35);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "비밀번호(PW)";
+            // 
+            // SignUpButton
+            // 
+            this.SignUpButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SignUpButton.BackColor = System.Drawing.Color.Black;
+            this.SignUpButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.SignUpButton.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.SignUpButton.Location = new System.Drawing.Point(333, 381);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(146, 39);
+            this.SignUpButton.TabIndex = 14;
+            this.SignUpButton.Text = "회원가입";
+            this.SignUpButton.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(494, 333);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 25);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(494, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 25);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.Button FindIdPw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SignUpButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
