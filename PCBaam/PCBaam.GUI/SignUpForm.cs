44297ccbb9c39213a0c2bcc6224fe7e1e712064c
inho1650 <jeong150@naver.com>
2018-10-16 11:01:35 +0900

    회원가입 디자인

--- PCBaam/PCBaam.GUI/SignUpForm.cs
+++ PCBaam/PCBaam.GUI/SignUpForm.cs
@@ -16,5 +16,30 @@ namespace PCBaam.GUI
         {
             InitializeComponent();
         }
+        private void SignUpForm_Load(object sender, EventArgs e)
+
+        {
+
+            comboBoxJob.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IdCheck id = new IdCheck();
+
+            id.Owner = this;
+
+            id.ShowDialog();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+ 
+        }
     }
 }
